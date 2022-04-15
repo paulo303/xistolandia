e96e7c50cc9366c5ae225114ac0e2d0a7c975ce2
Paulo Cavalcanti <paulocavalcanti303@gmail.com>
2022-04-15 01:26:16 -0300

    Migration e Seeds

---
+++ app/Http/Controllers/Admin/DjController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Dj;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class DjController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Dj  $dj
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Dj $dj)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Dj  $dj
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Dj $dj)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Dj  $dj
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Dj $dj)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Dj  $dj
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Dj $dj)
+    {
+        //
+    }
+}
