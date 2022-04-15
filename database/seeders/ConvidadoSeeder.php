975a0874e740503477f2e3a1bcf750cecfda6e6b
Paulo Cavalcanti <paulocavalcanti303@gmail.com>
2022-04-15 00:13:51 -0300

    Migrations / Seeds

---
+++ database/seeders/ConvidadoSeeder.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Convidado;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ConvidadoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Convidado::create(['nome' => 'Xisto', 'patrocinador' => 1]);
+        Convidado::create(['nome' => 'Tatiana']);
+        Convidado::create(['nome' => 'Geezer']);
+        Convidado::create(['nome' => 'Isa']);
+        Convidado::create(['nome' => 'Coruja']);
+        Convidado::create(['nome' => 'Camila Viana']);
+        Convidado::create(['nome' => 'Ju Galzerano']);
+        Convidado::create(['nome' => 'Japa Gustavo']);
+        Convidado::create(['nome' => 'Marcell Viana']);
+        Convidado::create(['nome' => 'Willian da Silva BOI']);
+        Convidado::create(['nome' => 'Claudinha Mainardez']);
+        Convidado::create(['nome' => 'Tuniko']);
+        Convidado::create(['nome' => 'Eli Possato']);
+        Convidado::create(['nome' => 'Ronaldo Cazuza']);
+        Convidado::create(['nome' => 'Daniela Domingues']);
+        Convidado::create(['nome' => 'Du Doc']);
+        Convidado::create(['nome' => 'Maria Estela']);
+        Convidado::create(['nome' => 'Juliana Onofre']);
+        Convidado::create(['nome' => 'Karina Teixeira']);
+        Convidado::create(['nome' => 'Daniel Foncek']);
+        Convidado::create(['nome' => 'Talita Gomes']);
+        Convidado::create(['nome' => 'Thiago Calabra']);
+        Convidado::create(['nome' => 'Lianny']);
+        Convidado::create(['nome' => 'Leticia Silva']);
+        Convidado::create(['nome' => 'Tiago Souza']);
+        Convidado::create(['nome' => 'Greice Ricon']);
+        Convidado::create(['nome' => 'Tiago Santos']);
+        Convidado::create(['nome' => 'Rafinha']);
+        Convidado::create(['nome' => 'Japa Fernando']);
+        Convidado::create(['nome' => 'Rafa Alemão']);
+        Convidado::create(['nome' => 'Ricardinho']);
+        Convidado::create(['nome' => 'Nori']);
+        Convidado::create(['nome' => 'Karen Fernanda']);
+        Convidado::create(['nome' => 'Chochi']);
+        Convidado::create(['nome' => 'Maju']);
+        Convidado::create(['nome' => 'Kowalski']);
+        Convidado::create(['nome' => 'Flavio Nakano']);
+        Convidado::create(['nome' => 'Joyce Rodrigues']);
+        Convidado::create(['nome' => 'Lolly Stardust']);
+        Convidado::create(['nome' => 'Thiago Machado']);
+        Convidado::create(['nome' => 'Leo Vilella']);
+        Convidado::create(['nome' => 'Popis']);
+        Convidado::create(['nome' => 'Parmitex']);
+        Convidado::create(['nome' => 'José Pedro Makowski']);
+        Convidado::create(['nome' => 'Camila Fernandes Cunha']);
+        Convidado::create(['nome' => 'Fab TR']);
+        Convidado::create(['nome' => 'Carol TR']);
+        Convidado::create(['nome' => 'Mou TR']);
+        Convidado::create(['nome' => 'Gisele TR']);
+        Convidado::create(['nome' => 'Pedro Cruz']);
+        Convidado::create(['nome' => 'Camila Laurindo']);
+        Convidado::create(['nome' => 'Matheus Rocha']);
+        Convidado::create(['nome' => 'Chiara Tiozzo']);
+        Convidado::create(['nome' => 'Nicole Dal']);
+        Convidado::create(['nome' => 'Paulo Mdazio']);
+        Convidado::create(['nome' => 'Débora Nascimento']);
+        Convidado::create(['nome' => 'Murilo Leão']);
+        Convidado::create(['nome' => 'Ricardo Ducco', 'patrocinador' => 1]);
+        Convidado::create(['nome' => 'Denise Ducco', 'patrocinador' => 1]);
+        Convidado::create(['nome' => 'João Pedro Schwab', 'patrocinador' => 1]);
+        Convidado::create(['nome' => 'Narjara', 'patrocinador' => 1]);
+        Convidado::create(['nome' => 'Sixteen', 'patrocinador' => 1]);
+        Convidado::create(['nome' => 'Tania Bouez', 'patrocinador' => 1]);
+        Convidado::create(['nome' => 'Manga', 'patrocinador' => 1]);
+        Convidado::create(['nome' => 'Felipe Neiva', 'patrocinador' => 1]);
+        Convidado::create(['nome' => 'Fabinho', 'patrocinador' => 1]);
+        Convidado::create(['nome' => 'Karin Campos', 'patrocinador' => 1]);
+        Convidado::create(['nome' => 'Tru Bueno', 'patrocinador' => 1]);
+        Convidado::create(['nome' => 'Cesar Goes', 'patrocinador' => 1]);
+        Convidado::create(['nome' => 'Aécio', 'patrocinador' => 1]);
+        Convidado::create(['nome' => 'Maria Paula', 'patrocinador' => 1]);
+        Convidado::create(['nome' => 'Xita', 'patrocinador' => 1]);
+        Convidado::create(['nome' => 'Cris Haick']);
+        Convidado::create(['nome' => 'João Miranda']);
+        Convidado::create(['nome' => 'Andrea Emboaba']);
+        Convidado::create(['nome' => 'Marcio Cagnato']);
+        Convidado::create(['nome' => 'Christine Shoeler T 70']);
+        Convidado::create(['nome' => 'Lea Cinthia Formigoli']);
+        Convidado::create(['nome' => 'Maria Carolina Androsoni']);
+        Convidado::create(['nome' => 'Ana Carolina Fortes']);
+        Convidado::create(['nome' => 'Eduardo Dimas']);
+        Convidado::create(['nome' => 'Marido do Eduardo Dimas']);
+        Convidado::create(['nome' => 'Adriana Manzi']);
+        Convidado::create(['nome' => 'Manzi']);
+        Convidado::create(['nome' => 'Fernando Luis Costa']);
+        Convidado::create(['nome' => 'Samuel Marinheiro']);
+        Convidado::create(['nome' => 'Virginia Biondi']);
+        Convidado::create(['nome' => 'Alex Mendes Pinto']);
+    }
+}
